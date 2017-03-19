0b9a8d41af9d4031486755ffbe7764f843c9f010
JoJoCoJo <jojocojo@msn.com>
2017-03-19 02:35:01 -0600

    Creación del API y ruta para Tabla Usuarios

--- src/routes.php
+++ src/routes.php
@@ -2,10 +2,53 @@
 // Routes
 
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/api', function ($request, $response, $args) {
 
-    // Render index view
     echo "¿qué onda ese?";
+});
+
+
+$app->group('/api/usuarios', function(){
+
+	/**
+	* Listar a todos los usuarios
+	*/
+	$this->get('/', function ($request, $response, $args){
+
+		$controller = new UsuarioController();
+		$json = $controller->callAction('all');
+
+		$code = ($json['code'] == 1) ? 200 : 401;
+		return $response->withJson($json, $code);
+
+	})->setName('lista_usuarios');
+
+	/**
+	* Insertar un usuario
+	*/
+	$this->post('/', function ($request, $response, $args){
+
+		$post = $request->getParams();
+
+		$controller = new UsuarioController();
+		$json = $controller->callAction('add', $post);
+
+		$code = ($json['code'] == 1) ? 200 : 401;
+		return $response->withJson($json, $code);
+
+	})->setName('insertar_usuario');
+
+	/**
+	* Buscar un usuario por su ID
+	*/
+	$this->get('/{id}/', function ($request, $response, $args){
+
+		$controller = new UsuarioController();
+		$json = $controller->callAction('one', $args['id']);
+
+		$code = ($json['code'] == 1) ? 200 : 401;
+		return $response->withJson($json, $code);
+
+	})->setName('usuario_by_id');
+
 });
