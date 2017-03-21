139f7bd271d5845399f28db135ab440514bddf0d
JoJoCoJo <jojocojo@msn.com>
2017-03-21 15:20:39 -0600

    Conexion a la otra BD

--- src/models/Connection.class.php
+++ src/models/Connection.class.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Capsule\Manager as DB;
 
 class Connection
 {
-	public static function conecting()	
-	{
+	public static function conecting ()	{
+
 		$fileName = __DIR__ . '/../config/conf.db.ini';
 		$config		=	parse_ini_file($fileName);
 		$connection = new DB;
@@ -16,6 +16,7 @@ class Connection
 		$connection->setAsGlobal();
 		$connection->bootEloquent();
 		return $connection;
+
 	}
 
 	public static function getConnection () {
@@ -28,5 +29,30 @@ class Connection
 		$connection->bootEloquent();
 
 		return $connection;			
+	}
+
+	public static function conectingAPI () {
+
+		$fileNameAPI = __DIR__ . '/../config/conf2.db.ini';
+		$configAPI		=	parse_ini_file($fileNameAPI);
+		$connectionAPI = new DB;
+		$connectionAPI->addConnection($configAPI);
+		$connectionAPI->setAsGlobal();
+		$connectionAPI->bootEloquent();
+		return $connectionAPI;
+
+	}
+
+	public static function getConnectionAPI () {
+
+		$fileNameAPI	= __DIR__ . '/../config/conf2.db.ini';
+		$configAPI	=	parse_ini_file( $fileNameAPI );
+		$connectionAPI = new DB;
+		$connectionAPI->addConnection( $configAPI );
+		$connectionAPI->setAsGlobal();
+		$connectionAPI->bootEloquent();
+
+		return $connectionAPI;
+
 	}
 }
