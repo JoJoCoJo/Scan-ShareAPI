d9cde9e04294e7858af36a3b035324602132cced
JoJoCoJo <jojocojo@msn.com>
2017-03-31 16:26:11 -0600

    Nuevas rutas agregadas de Targets y retorno de algunas funciones como JSONArray y no JSONObject utilizados por la App Móvil

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,9 @@ $app->get('/api', function ($request, $response, $args) {
     echo "¿qué onda ese?";
 });
 
-
+/**
+* Rutas para los usuarios
+*/
 $app->group('/api/usuarios', function(){
 
 	/**
@@ -22,6 +24,19 @@ $app->group('/api/usuarios', function(){
 		return $response->withJson($json, $code);
 
 	})->setName('lista_usuarios');
+
+	/**
+	* Listar a todos los usuarios API
+	*/
+	$this->get('/android/list/', function ($request, $response, $args){
+
+		$controller = new UsuarioController();
+		$json = $controller->callAction('allAPI');
+
+		$code = ($json['code'] == 1) ? 200 : 401;
+		return $response->withJson($json, $code);
+
+	})->setName('lista_usuarios_android');
 
 	/**
 	* Insertar un usuario
@@ -49,7 +64,7 @@ $app->group('/api/usuarios', function(){
 		$json = $controller->callAction('addAPI', $post);
 
 		$code = ($json['code'] == 1) ? 200 : 401;
-		return $response->withJson($json, $code);
+		return $response->withJson(array($json), $code);
 
 	})->setName('insertar_usuario_api');
 
@@ -109,5 +124,40 @@ $app->group('/api/usuarios', function(){
 		return $response->withJson(array($json), $code);
 
 	})->setName('autenticacion_usuario');
+
+});
+
+
+/**
+* Rutas para los targets
+*/
+$app->group('/api/targets', function(){
+
+	/**
+	* Listar a todos los targets
+	*/
+	$this->post('/', function ($request, $response, $args){
+
+		$controller = new TargetController();
+		$json = $controller->callAction('all');
+
+		$code = ($json['code'] == 1) ? 200 : 401;
+		return $response->withJson(array($json), $code);
+
+	})->setName('lista_targets');
+
+	/**
+	* Medir a distancia entre la posición actual y el target más cercano
+	*/
+	$this->post('/medir/', function ($request, $response, $args){
+
+		$controller = new TargetController();
+		$params = $request->getParams();
+		$json = $controller->callAction('cerca', $params);
+
+		$code = ($json['code'] == 1) ? 200 : 401;
+		return $response->withJson(array($json), $code);
+
+	})->setName('medir_distancia_target');
 
 });
