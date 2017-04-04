af47781d9744057accedb67c71aaf9713c5fb2a8
JoJoCoJo <jojocojo@msn.com>
2017-04-04 12:05:59 -0500

    Vista login

---
+++ public/views/loginView.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<link rel="stylesheet" type="text/css" href="/css/materialize.css">
+	<title> Login | Plataforma </title>
+	<script type="text/javascript" src="/js/materialize.js"></script>
+	<script type="text/javascript" src="/js/jquery-2.1.1.js"></script>
+</head>
+<body>
+	<form action="" method="post">
+		<label for="email">Usuario: </label>
+		<input type="text" name="email" id="email" placeholder="Ingrese su nombre de Usuario." required>
+		<br/>
+		<label for="password">Contraseña: </label>
+		<input type="password" name="password" id="password" placeholder="Ingrese su Contraseña." required>
+		<br/>
+		<input type="submit" value="Ingresar">
+	</form>
+</body>
+</html>
