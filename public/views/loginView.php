b49ec5a236bbbebdf6af898989fce1e80bb608aa
JoJoCoJo <jojocojo@msn.com>
2017-04-17 17:36:26 -0500

    Modificaciones

--- public/views/loginView.php
+++ public/views/loginView.php
@@ -5,10 +5,10 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
 	<link rel="stylesheet" type="text/css" href="/css/materialize.css">
 	<link rel="stylesheet" type="text/css" href="/font-awesome-4.7.0/css/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="/sweetalert2/sweetalert2.css">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
 
 	<title> Login | Plataforma </title>
 
